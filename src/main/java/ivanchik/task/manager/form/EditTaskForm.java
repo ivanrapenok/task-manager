b43e0daba10624c6cc01c966173e5658cc458cb0
Ivan <isostarspb@gmail.com>
2016-01-05 18:28:34 +0300

    ver 0 -i

---
+++ src/main/java/ivanchik/task/manager/form/EditTaskForm.java
@@ -0,0 +1,302 @@
+package ivanchik.task.manager.form;
+
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+import ivanchik.task.manager.api.db.Storage;
+import ivanchik.task.manager.api.pojo.Task;
+import ivanchik.task.manager.core.db.H2Storage;
+import net.sourceforge.jdatepicker.impl.JDatePanelImpl;
+import net.sourceforge.jdatepicker.impl.JDatePickerImpl;
+import net.sourceforge.jdatepicker.impl.UtilDateModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
+public class EditTaskForm extends JFrame implements Runnable{
+
+    private JLabel parentTask;
+    private JLabel taskName;
+    private JLabel description;
+    private JLabel comment;
+    private JLabel startDate;
+    private JLabel endDate;
+    private JLabel progress;
+    private JLabel timeSpent;
+    private JLabel daly;
+    private JLabel closed;
+
+    private JTextField parentTaskField;
+    private JTextField taskNameField;
+    private JTextField descriptionField;
+    //private JDatePickerImpl commentField;
+    private JDatePickerImpl startDateField;
+    private JDatePickerImpl endDateField;
+    private JTextField progressField;
+    private JTextField timeSpentField;
+    private JCheckBox dalyField;
+    private JCheckBox closedField;
+
+    private JPanel addButtonPanel;
+    private JPanel editButtonPannel;
+    private JButton addButton;
+    private JButton editButton;
+    private JButton deleteButton;
+    private JButton cancelButton;
+
+    private final Storage storage = H2Storage.getInstance();
+    private long userId;
+    private Task task;
+
+    public EditTaskForm(boolean editMode, long userId, Task task) {
+        this.userId = userId;
+        this.task = task;
+        initComponents(editMode);
+    }
+
+    private void initComponents(boolean editMode) {
+
+        System.out.println(task.getParentId());
+
+        parentTask = new JLabel("Parent task");
+        taskName = new JLabel("Task name");
+        description = new JLabel("Description");
+        comment = new JLabel("Comment");
+        startDate = new JLabel("Start date");
+        endDate = new JLabel("End date");
+        progress = new JLabel("Progress");
+        timeSpent = new JLabel("Spent time");
+        daly = new JLabel("Daly");
+        closed = new JLabel("Closed");
+
+        parentTaskField = new JTextField();
+        taskNameField = new JTextField();
+        descriptionField = new JTextField();
+
+        UtilDateModel model = new UtilDateModel();
+        model.setSelected(true);
+        Calendar calendar = Calendar.getInstance();
+        model.setDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        JDatePanelImpl datePanel = new JDatePanelImpl(model);
+        //commentField = new JDatePickerImpl(datePanel);
+        startDateField = new JDatePickerImpl(datePanel);
+        endDateField = new JDatePickerImpl(datePanel);
+        progressField = new JTextField();
+        timeSpentField = new JTextField();
+        dalyField = new JCheckBox();
+        closedField = new JCheckBox();
+
+        addButtonPanel = new JPanel();
+        editButtonPannel = new JPanel();
+        addButton = new JButton("Add");
+        editButton = new JButton("Edit");
+        deleteButton = new JButton("Delete");
+        cancelButton = new JButton("Cancel");
+
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new FormLayout(
+                "50px, default, 300, 50px",
+                "default, default, default, default, default, " +
+                        "default, default, default, default, " +
+                        "default, default, default, default, " +
+                        "default, default"));
+
+
+        parentTask.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(parentTask, new CellConstraints(2, 1));
+
+        parentTaskField.enable(false);
+        parentTaskField.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(parentTaskField, new CellConstraints(3, 1));
+
+        taskName.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(taskName, new CellConstraints(2, 2));
+
+        taskNameField.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(taskNameField, new CellConstraints(3, 2));
+
+        description.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(description, new CellConstraints(2, 3));
+
+        descriptionField.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(descriptionField, new CellConstraints(3, 3));
+
+        comment.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(comment, new CellConstraints(2, 4));
+
+//        commentField.setPreferredSize(new Dimension(100, 30));
+//        contentPane.add(commentField, new CellConstraints(3, 4));
+
+        startDate.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(startDate, new CellConstraints(2, 5));
+
+        startDateField.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(startDateField, new CellConstraints(3, 5));
+
+        endDate.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(endDate, new CellConstraints(2, 6));
+
+        endDateField.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(endDateField, new CellConstraints(3, 6));
+
+        endDate.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(endDate, new CellConstraints(2, 7));
+
+        endDateField.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(endDateField, new CellConstraints(3, 7));
+
+        progress.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(progress, new CellConstraints(2, 8));
+
+        progressField.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(progressField, new CellConstraints(3, 8));
+
+        timeSpent.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(timeSpent, new CellConstraints(2, 9));
+
+        timeSpentField.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(timeSpentField, new CellConstraints(3, 9));
+
+        daly.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(daly, new CellConstraints(2, 10));
+
+        dalyField.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(dalyField, new CellConstraints(3, 10));
+
+        closed.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(closed, new CellConstraints(2, 11));
+
+        closedField.setPreferredSize(new Dimension(100, 30));
+        contentPane.add(closedField, new CellConstraints(3, 11));
+
+
+        editButton.setPreferredSize(new Dimension(75, 25));
+        deleteButton.setPreferredSize(new Dimension(75, 25));
+        addButton.setPreferredSize(new Dimension(75, 25));
+        cancelButton.setPreferredSize(new Dimension(75, 25));
+
+        if (editMode) {
+            if (task.getDescription() != null) {
+                descriptionField.setText(task.getDescription());
+            }
+            if (task.getEndDate() != null) {
+                endDateField = setDateJDatePicker(endDateField, task.getEndDate());
+            }
+            if (task.getName() != null) {
+                taskNameField.setText(task.getName());
+            }
+            if (task.getParentId() != null) {
+                parentTaskField.setText(task.getParentId().toString());
+            }
+            if (task.getStartDate() != null) {
+                startDateField = setDateJDatePicker(startDateField, task.getStartDate());
+            }
+            if (task.getParentId() != null) {
+                parentTaskField.setText(task.getParentId().toString());
+            }
+            progressField.setText(String.valueOf(task.getProgress()));
+            timeSpentField.setText(String.valueOf(task.getTimeSpent()));
+            dalyField.setSelected(task.isDaily());
+            closedField.setSelected(task.isClosed());
+
+            editButtonPannel.add(editButton);
+            editButtonPannel.add(deleteButton);
+            editButtonPannel.add(cancelButton);
+            contentPane.add(editButtonPannel, new CellConstraints(1, 13, 4, 1));
+        } else {
+            if (task.getParentId() != null) {
+                parentTaskField.setText(task.getParentId().toString());
+            }
+            addButtonPanel.add(addButton);
+            addButtonPanel.add(cancelButton);
+            contentPane.add(addButtonPanel, new CellConstraints(2, 12, 2, 1));
+        }
+
+
+        addButton.addActionListener(this::addTask);
+        cancelButton.addActionListener(this::cancel);
+        deleteButton.addActionListener(this::deleteTask);
+        editButton.addActionListener(this::editTask);
+
+        pack();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(getOwner());
+        setVisible(true);
+    }
+
+    public void cancel(ActionEvent actionEvent) {
+        setVisible(false);
+        dispose();
+        SwingUtilities.invokeLater(new TaskForm(userId));
+    }
+
+    public void addTask(ActionEvent actionEvent) {
+        storage.addTask(fillTask());
+        setVisible(false);
+        dispose();
+        SwingUtilities.invokeLater(new TaskForm(userId));
+    }
+
+    public void editTask(ActionEvent actionEvent) {
+        storage.updateTask(fillTask());
+        setVisible(false);
+        dispose();
+        SwingUtilities.invokeLater(new TaskForm(userId));
+    }
+
+    public void deleteTask(ActionEvent actionEvent) {
+        storage.deleteTask(task);
+        setVisible(false);
+        dispose();
+        SwingUtilities.invokeLater(new TaskForm(userId));
+    }
+
+    public JDatePickerImpl setDateJDatePicker(JDatePickerImpl jDatePicker, Timestamp timestamp) {
+        try {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(timestamp.getTime());
+            jDatePicker.getModel().setYear(calendar.get(Calendar.YEAR));
+            jDatePicker.getModel().setMonth(calendar.get(Calendar.MONTH));
+            jDatePicker.getModel().setDay(calendar.get(Calendar.DAY_OF_MONTH));
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return jDatePicker;
+    }
+
+    public Timestamp dateToTimeStamp(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return new Timestamp(calendar.getTimeInMillis());
+    }
+
+    public Task fillTask() {
+        Task filledTask = new Task().setName(taskNameField.getText()).
+                setClosed(closedField.isSelected()).
+                setDaily(dalyField.isSelected()).
+                setDescription(descriptionField.getText()).
+                setUserId(userId).
+                setId(task.getId()).
+                setEndDate(dateToTimeStamp((Date) endDateField.getModel().getValue())).
+                setStartDate(dateToTimeStamp((Date) startDateField.getModel().getValue())).
+                setProgress(Integer.parseInt(progressField.getText())).
+                setTimeSpent(Integer.parseInt(timeSpentField.getText()));
+        if (!parentTaskField.getText().isEmpty()) {
+            filledTask.setParentId(Long.valueOf(parentTaskField.getText()));
+        }
+        return filledTask;
+    }
+
+    public boolean checkFields() {
+
+        return true;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/main/java/ivanchik/task/manager/form/TaskForm.java
@@ -0,0 +1,143 @@
+package ivanchik.task.manager.form;
+
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+import ivanchik.task.manager.api.model.NotEditableTableModel;
+import ivanchik.task.manager.api.db.Storage;
+import ivanchik.task.manager.api.pojo.Task;
+import ivanchik.task.manager.core.db.H2Storage;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class TaskForm extends JFrame implements Runnable {
+
+    private JButton createSubTaskButton;
+    private JButton editButton;
+    private JButton historyButton;
+    private JScrollPane scrollPane1;
+    private JTable taskTable;
+    private JButton createTaskButton;
+    private JButton signOutButton;
+
+    private final Storage storage = H2Storage.getInstance();
+    private long userId;
+
+    public TaskForm(long userId) {
+        this.userId = userId;
+        initComponents(userId);
+        taskTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        fillUserTable();
+    }
+
+    private void initComponents(long userId) {
+
+        createTaskButton = new JButton();
+        createSubTaskButton = new JButton();
+        editButton = new JButton();
+        historyButton = new JButton();
+        signOutButton = new JButton();
+        scrollPane1 = new JScrollPane();
+        taskTable = new JTable();
+
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new FormLayout(
+                "default, default",
+                "default, default, default, default, default, 200px"));
+
+        createTaskButton.setText("Add task");
+        contentPane.add(createTaskButton, new CellConstraints(2, 1));
+
+        createSubTaskButton.setText("Add subtask");
+        contentPane.add(createSubTaskButton, new CellConstraints(2, 2));
+
+        editButton.setText("Edit");
+        contentPane.add(editButton, new CellConstraints(2, 3));
+
+        historyButton.setText("Show history");
+        contentPane.add(historyButton, new CellConstraints(2, 4));
+
+        signOutButton.setText("Sign out");
+        contentPane.add(signOutButton, new CellConstraints(2, 5));
+
+        scrollPane1.setViewportView(taskTable);
+        contentPane.add(scrollPane1, new CellConstraints(1, 1, 1, 6));
+
+        historyButton.addActionListener(this::showHideHistory);
+        createTaskButton.addActionListener(this::addTask);
+        createSubTaskButton.addActionListener(this::addSubTask);
+        editButton.addActionListener(this::editTask);
+        signOutButton.addActionListener(this::signOut);
+
+        pack();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(getOwner());
+        setVisible(true);
+    }
+
+    private void addTask(ActionEvent actionEvent) {
+        setVisible(false);
+        dispose();
+        SwingUtilities.invokeLater(new EditTaskForm(false, userId, new Task()));
+    }
+
+    private void editTask(ActionEvent actionEvent) {
+        int selected = taskTable.getSelectedRow();
+        if(selected < 0) {
+            return;
+        }
+        long id = (long) taskTable.getModel().getValueAt(selected, 0);
+        Task task = storage.getTask(new Task().setId(id));
+        setVisible(false);
+        dispose();
+        SwingUtilities.invokeLater(new EditTaskForm(true, userId, task));
+    }
+
+    private void addSubTask(ActionEvent actionEvent) {
+        int selected = taskTable.getSelectedRow();
+        if(selected < 0) {
+            return;
+        }
+        long id = (long) taskTable.getModel().getValueAt(selected, 0);
+        setVisible(false);
+        dispose();
+        SwingUtilities.invokeLater(new EditTaskForm(false, userId, new Task().setParentId(id)));
+    }
+
+    private void showHideHistory(ActionEvent e) {
+
+    }
+
+    private void signOut(ActionEvent actionEvent) {
+        setVisible(false);
+        dispose();
+        SwingUtilities.invokeLater(MainForm::new);
+    }
+
+    private void fillUserTable() {
+        Object[] columnNames = {"id", "name", "description", "end date", "progress", "time spent"};
+        DefaultTableModel model = new NotEditableTableModel(columnNames, 0);
+        for (Task task : storage.getTaskList()) {
+            if ((task.getUserId() != null) && (task.getUserId() == userId)) {
+                model.addRow(new Object[]{task.getId(),
+                        task.getName(),
+                        task.getDescription(),
+                        task.getEndDate(),
+                        task.getProgress(),
+                        task.getTimeSpent(),
+                        task.getParentId(),
+                        task.getStartDate(),
+                        task.getUserId()});
+            }
+        }
+        taskTable.setModel(model);
+        taskTable.removeColumn(taskTable.getColumnModel().getColumn(0));
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
